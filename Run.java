d52a198ed402e1dd4aac038272de2785bf67bd19
Jozdien <43042151+Jozdien@users.noreply.github.com>
2019-08-24 22:06:01 +0530

    Add files via upload

---
+++ Run.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+class Run
+{
+	public static void main()
+	{
+		try
+		{
+			System.out.println(s);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Syntax Error, mate. Check again. In the meantime, here's a fun fact to keep you occupied: ");
+			System.out.println("One of the guys in charge here, is definitely trying to kill you. Good luck figuring out who it is.");
+
+		}
+	}
+}
